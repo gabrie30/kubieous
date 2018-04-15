a3001bba23557a043d766883eb8e28919b601257
Jay Gabriels <jgabriels30@gmail.com>
2018-04-15 09:12:37 -0700

    improve hpa

--- checks/hpa.go
+++ checks/hpa.go
@@ -3,11 +3,13 @@ package checks
 import (
 	"fmt"
 
+	"k8s.io/api/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
+// HPA calls checks for horizontal pod autoscalers
 func HPA() {
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
@@ -21,7 +23,23 @@ func HPA() {
 		panic(err.Error())
 	}
 
-	hpa, _ := clientset.AutoscalingV1().HorizontalPodAutoscalers("").List(metav1.ListOptions{})
+	// hpas, err := clientset.AutoscalingV1().HorizontalPodAutoscalers("").List(metav1.ListOptions{})
+	// hpaClient := clientset.AutoscalingV1().RESTClient
+	// meta.NewAccessor().Namespace(data.Object)
 
-	fmt.Println(hpa)
+	hpaWatch, _ := clientset.AutoscalingV1().HorizontalPodAutoscalers("").Watch(metav1.ListOptions{})
+
+	data := <-hpaWatch.ResultChan()
+
+	hpa := data.Object.(*v1.HorizontalPodAutoscaler)
+
+	if hpa.Status.CurrentReplicas > hpa.Status.DesiredReplicas {
+		fmt.Println(hpa.ObjectMeta.Namespace + " is scaling DOWN")
+		fmt.Println("Current VS Desired Replicas: ", hpa.Status.CurrentReplicas, hpa.Status.DesiredReplicas)
+		fmt.Println("Current CPU utilization: ", hpa.Status.CurrentCPUUtilizationPercentage)
+	} else if hpa.Status.CurrentReplicas < hpa.Status.DesiredReplicas {
+		fmt.Println(hpa.ObjectMeta.Namespace + " is scaling up")
+		fmt.Println("Current VS Desired Replicas: ", hpa.Status.CurrentReplicas, hpa.Status.DesiredReplicas)
+		fmt.Println("Current CPU utilization: ", hpa.Status.CurrentCPUUtilizationPercentage)
+	}
 }
